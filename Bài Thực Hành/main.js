cab8838759eded8867c90b027be0bd4c96e9c05f
Nguyen Van Duy <duyyb257946@gmail.com>
2022-04-29 08:22:54 +0700

    bai thuc hanh

---
+++ Bài Thực Hành/main.js
@@ -0,0 +1,24 @@
+// Bài 1
+
+function ShowMessage() {
+    alert('Xin chào');
+}
+
+// bài 2 */
+
+// function  ShowMessage() {
+//     document.write('Xin chào');
+// }
+
+//bài 3 */
+
+// function ShowMessage() {
+//     document.write('Xin chào')
+// }
+
+
+// Bài 4 */
+
+// function  ShowMessage() {
+//     console.log('Xin chào');
+// }
